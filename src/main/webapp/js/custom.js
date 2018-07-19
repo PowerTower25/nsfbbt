a9f310e29cea156285e902632d1fc7581db98037
Grady <jgrady@bbandt.com>
2018-07-19 11:32:46 -0400

    Added Ajax call to populate user data

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,7 +1,31 @@
-// $(document).ready( function() {
-//   $('[data-toggle="slide-collapse"]').on('click', function() {
-//       $navMenuCont = $($(this).data('target'));
-//       console.log($navMenuCont)
-//       $navMenuCont.animate({'width':'toggle'}, 1000);
-//   });
-// });
+$(document).ready( function() {
+  $('.js-list-hide').hide()
+  $('.js-get-data').on('click', function() {
+    $.ajax({
+
+        'url' : 'https://bbtnsf.mybluemix.net/insight?email=rose@pedalflowers.biz',
+        'type' : 'GET',
+        success : function(data) {
+          $('.js-list-hide').show();
+          // console.log(data.email)
+          // console.log(data.alerts[0].product)
+          for ( var i = 0; i <  data.alerts.length; i++ ) {
+            // console.log(i)
+            $('.js-alert')[i].append(data.alerts[i].product)
+            $('.js-prob')[i].append(data.alerts[i].problem)
+            $('.js-sug')[i].append(data.alerts[i].suggestion)
+            // console.log(data.alerts)
+            // if ( data.hasOwnProperty( i ) ) {
+            //   console.log(i)
+            // }
+          }
+
+            // alert(data);
+        },
+        'error' : function(request,error)
+        {
+            // alert("Request: "+JSON.stringify(request));
+        }
+    });
+  });
+});
